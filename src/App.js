405b95120ae58ee9dfe5c20c42f82a0b927f7a22
zafajardo9 <zack.csgo9@gmail.com>
2020-09-06 08:26:02 +0800

    First

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <div className="App">
+      <h1>Zacks</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../App.css'
+
+export default function Navbar() {
+    return (
+        <div className="Navbar">
+            <h1>Title</h1>
+            <div className="link"></div>
+        </div>
+    )
+}
